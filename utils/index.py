aaa580b48ceeee73e3690bde202adbaa727db5cb
draconacht <184783355+draconacht@users.noreply.github.com>
2025-04-22 01:47:40 +0530

    godforsaken fuzzy matching

--- utils/index.py
+++ utils/index.py
@@ -1,5 +1,7 @@
-import difflib
+from functools import lru_cache
 from typing import Callable
+
+import rapidfuzz
 
 
 class Index[T]:
@@ -11,12 +13,27 @@ class Index[T]:
 
 	def __init__(self, items: list[T], namegetter: Callable[[T], str], aliases: dict[str, int]):
 		self.items = items
-		self.lookup_dict = {namegetter(x).lower(): x for x in items if x is not None} | {K.lower(): items[V] for K, V in aliases.items()}
+		self.lookup_dict = ({namegetter(x).lower(): x for x in items if x is not None}
+												| {K.lower(): items[V] for K, V in aliases.items()})
 		self.namegetter = namegetter
 
+	@lru_cache(maxsize=1 << 10)
 	def lookup(self, target: str) -> T:
-		name = difflib.get_close_matches(target.lower(), self.lookup_dict.keys(), cutoff=0)[0]
-		return self.lookup_dict[name]
+		name, score, key = rapidfuzz.process.extractOne(target, self.lookup_dict.keys(), scorer=rapidfuzz.fuzz.QRatio)
+		if score > 85:
+			return self.lookup_dict[name]
+		else:
+			name, score, key = rapidfuzz.process.extractOne(target, self.lookup_dict.keys())
+			return self.lookup_dict[name]
+
+	@lru_cache(maxsize=1 << 10)
+	def lookup_with_score(self, target: str) -> tuple[T, float]:
+		name, score, key = rapidfuzz.process.extractOne(target, self.lookup_dict.keys(), scorer=rapidfuzz.fuzz.QRatio)
+		if score > 85:
+			return self.lookup_dict[name], score
+		else:
+			name, _, key = rapidfuzz.process.extractOne(target, self.lookup_dict.keys())
+			return self.lookup_dict[name], rapidfuzz.fuzz.QRatio(target, name)
 
 	def get(self, id_: int) -> T:
 		return self.items[id_]
