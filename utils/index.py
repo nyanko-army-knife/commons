650258f4d8b42b21f7571d0e91fc65307b638c46
draconacht <184783355+draconacht@users.noreply.github.com>
2025-05-22 01:26:46 +0530

    added debug name search, interactive stage lookup, stage with ID

--- utils/index.py
+++ utils/index.py
@@ -1,7 +1,18 @@
+import typing
 from functools import lru_cache
 from typing import Callable
 
 import rapidfuzz
+
+
+class Lookup(typing.NamedTuple):
+	name: str
+	score: float
+	key: str
+
+
+QUICK_THRESHOLD = 80
+SLOW_THRESHOLD = 70
 
 
 class Index[T]:
@@ -21,7 +32,7 @@ class Index[T]:
 	def lookup(self, target: str) -> T:
 		# noinspection PyTypeChecker
 		name, score, key = rapidfuzz.process.extractOne(target, self.lookup_dict.keys(), scorer=rapidfuzz.fuzz.QRatio)
-		if score > 85:
+		if score >= QUICK_THRESHOLD:
 			return self.lookup_dict[name]
 		else:
 			name, score, key = rapidfuzz.process.extractOne(target, self.lookup_dict.keys())
@@ -31,11 +42,22 @@ class Index[T]:
 	def lookup_with_score(self, target: str) -> tuple[T, float]:
 		# noinspection PyTypeChecker
 		name, score, key = rapidfuzz.process.extractOne(target, self.lookup_dict.keys(), scorer=rapidfuzz.fuzz.QRatio)
-		if score > 85:
+		if score >= QUICK_THRESHOLD:
 			return self.lookup_dict[name], score
 		else:
 			name, _, key = rapidfuzz.process.extractOne(target, self.lookup_dict.keys())
 			return self.lookup_dict[name], rapidfuzz.fuzz.QRatio(target, name)
+
+	@lru_cache(maxsize=1 << 10)
+	def lookup_debug(self, target: str, force_quick: bool = False) -> (bool, list[Lookup]):
+		# noinspection PyTypeChecker
+		lookups: list[Lookup] = [Lookup(*x) for x in
+														 rapidfuzz.process.extract(target, self.lookup_dict.keys(), scorer=rapidfuzz.fuzz.QRatio)]
+		if lookups[0].score >= QUICK_THRESHOLD or force_quick:
+			return True, lookups
+
+		lookups = [Lookup(*x) for x in rapidfuzz.process.extract(target, self.lookup_dict.keys())]
+		return False, lookups
 
 	def get(self, id_: int) -> T:
 		return self.items[id_]
